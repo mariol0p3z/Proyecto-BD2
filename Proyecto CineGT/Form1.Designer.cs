e2d8693f57cde413232e8c19eb3a1e8570737e3f
Mario <maronlopez60@gmail.com>
2024-10-23 18:38:19 -0600

    Primer Commit

--- Proyecto CineGT/Form1.Designer.cs
+++ Proyecto CineGT/Form1.Designer.cs
@@ -66,6 +66,7 @@
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(100, 22);
             this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
             // 
             // btnIncioSesion
             // 
