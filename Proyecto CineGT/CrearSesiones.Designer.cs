88897086595b067bcfd78ebe7e990c601302952c
di3gorivas <DMRIVASA@correo.url.edu.gt>
2024-11-03 18:02:53 -0600

    Avances formularios FrontEnd

--- Proyecto CineGT/CrearSesiones.Designer.cs
+++ Proyecto CineGT/CrearSesiones.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proyecto_CineGT
 {
-    partial class Historial
+    partial class CrearSesiones
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CrearSesiones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Historial";
+            this.Name = "CrearSesiones";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
